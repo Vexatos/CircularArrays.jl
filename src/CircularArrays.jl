14392b9fd7b52bc8340953bb6a8e9f9b412676f8
Yuval <yharel@campus.technion.ac.il>
2019-12-29 13:18:38 +0200

    Generalized indices and arbitrary indexing support. (#1)
    * Generalized indices and arbitrary indexing support.
    
    * Moved array creation inside test set
    
    * more matrix tests. renamed (i,j)=>(x,y)
    
    * Removed OffsetArrays dependency
    
    * Comment tweak

--- src/CircularArrays.jl
+++ src/CircularArrays.jl
@@ -26,15 +26,31 @@ Alias for [`CircularArray{T,1}`](@ref).
 """
 const CircularVector{T} = CircularArray{T, 1}
 
-@inline clamp_bounds(arr::CircularArray, I::Tuple{Vararg{Int}})::AbstractArray{Int, 1} = map(dim -> mod1(I[dim], size(arr.data, dim)), eachindex(I))
+# Copied from a method of Base.mod, for compatibility with Julia version < 1.3,
+# where this method is not defined
+_mod(i::Integer, r::AbstractUnitRange{<:Integer}) = mod(i-first(r), length(r)) + first(r)
+@inline clamp_bounds(arr::CircularArray, I::Tuple{Vararg{Int}})::AbstractArray{Int, 1} = map(Base.splat(_mod), zip(I, axes(arr.data)))
 
 CircularArray(def::T, size) where T = CircularArray(fill(def, size))
 
-@inline Base.getindex(arr::CircularArray, i::Int) = @inbounds getindex(arr.data, mod1(i, size(arr.data, 1)))
-@inline Base.setindex!(arr::CircularArray, v, i::Int) = @inbounds setindex!(arr.data, v, mod1(i, size(arr.data, 1)))
+@inline Base.getindex(arr::CircularArray, i::Int) = @inbounds getindex(arr.data, mod(i, Base.axes1(arr.data)))
+@inline Base.setindex!(arr::CircularArray, v, i::Int) = @inbounds setindex!(arr.data, v, mod(i, Base.axes1(arr.data)))
 @inline Base.getindex(arr::CircularArray, I::Vararg{Int}) = @inbounds getindex(arr.data, clamp_bounds(arr, I)...)
 @inline Base.setindex!(arr::CircularArray, v, I::Vararg{Int}) = @inbounds setindex!(arr.data, v, clamp_bounds(arr, I)...)
 @inline Base.size(arr::CircularArray) = size(arr.data)
+@inline Base.axes(arr::CircularArray) = axes(arr.data)
+
+@inline Base.checkbounds(::CircularArray, _...) = nothing
+
+@inline _similar(arr::CircularArray, ::Type{T}, dims) where T = CircularArray(similar(arr.data,T,dims))
+@inline Base.similar(arr::CircularArray, ::Type{T}, dims::Tuple{Base.DimOrInd, Vararg{Base.DimOrInd}}) where T = _similar(arr,T,dims)
+# Ambiguity resolution with Base
+@inline Base.similar(arr::CircularArray, ::Type{T}, dims::Tuple{Int64,Vararg{Int64}}) where T = _similar(arr,T,dims)
+# Ambiguity resolution with a type-pirating OffsetArrays method. See OffsetArrays issue #87.
+# Ambiguity is triggered in the case similar(arr) where arr.data::OffsetArray.
+# The OffsetAxis definition is copied from OffsetArrays.
+const OffsetAxis = Union{Integer, UnitRange, Base.OneTo, Base.IdentityUnitRange, Colon}
+@inline Base.similar(arr::CircularArray, ::Type{T}, dims::Tuple{OffsetAxis, Vararg{OffsetAxis}}) where T = _similar(arr,T,dims)
 
 CircularVector(data::AbstractArray{T, 1}) where T = CircularVector{T}(data)
 CircularVector(def::T, size::Int) where T = CircularVector{T}(fill(def, size))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,85 @@
 using CircularArrays
+using OffsetArrays
 using Test
-
-v1 = CircularVector(rand(Int64, 5))
 
 @test IndexStyle(CircularArray) == IndexCartesian()
 @test IndexStyle(CircularVector) == IndexLinear()
 
-@test size(v1, 1) == 5
-@test typeof(v1) == CircularVector{Int64}
-@test isa(v1, CircularVector)
-@test isa(v1, AbstractVector{Int})
-@test !isa(v1, AbstractVector{String})
-@test v1[2] == v1[2 + length(v1)]
-v1[2] = 0
-v1[3] = 0
-@test v1[2] == v1[3]
-@test_throws MethodError v1[2] = "Hello"
+@testset "vector" begin
+    data = rand(Int64, 5)
+    v1 = CircularVector(data)
 
-v2 = CircularVector("abcde", 5)
+    @test size(v1, 1) == 5
+    @test typeof(v1) == CircularVector{Int64}
+    @test isa(v1, CircularVector)
+    @test isa(v1, AbstractVector{Int})
+    @test !isa(v1, AbstractVector{String})
+    @test v1[2] == v1[2 + length(v1)]
 
-@test prod(v2) == "abcde"^5
+    @test v1[0] == data[end]
+    @test v1[-4:10] == [data; data; data]
+    @test v1[-3:1][-1] == data[end]
+    @test v1[[true,false,true,false,true]] == v1[[1,3,0]]
 
-@test_throws MethodError push!(v1, 15)
+    v1copy = copy(v1)
+    v1_2 = v1[2]
+    v1[2] = 0
+    v1[3] = 0
+    @test v1[2] == v1[3] == 0
+    @test v1copy[2] == v1_2
+    @test v1copy[7] == v1_2
+    @test_throws MethodError v1[2] = "Hello"
 
-b_arr = [2 4 6 8; 10 12 14 16; 18 20 22 24]
-a1 = CircularArray(b_arr)
-@test size(a1) == (3, 4)
-@test a1[2, 3] == 14
-a1[2, 3] = 17
-@test a1[2, 3] == 17
-@test !isa(a1, CircularVector)
-@test !isa(a1, AbstractVector)
-@test isa(a1, AbstractArray)
+    v2 = CircularVector("abcde", 5)
 
-@test size(reshape(a1, (2, 2, 3))) == (2, 2, 3)
+    @test prod(v2) == "abcde"^5
 
-a2 = CircularArray(4, (2, 3))
-@test isa(a2, CircularArray{Int, 2})
+    @test_throws MethodError push!(v1, 15)
+end
+
+@testset "matrix" begin
+    b_arr = [2 4 6 8; 10 12 14 16; 18 20 22 24]
+    a1 = CircularArray(b_arr)
+    @test size(a1) == (3, 4)
+    @test a1[2, 3] == 14
+    a1[2, 3] = 17
+    @test a1[2, 3] == 17
+    @test a1[-1, 7] == 17
+    @test a1[-1:5, 4:10][1, 4] == 17
+    @test a1[:, -1:-1][2, 1] == 17
+    @test !isa(a1, CircularVector)
+    @test !isa(a1, AbstractVector)
+    @test isa(a1, AbstractArray)
+
+    @test size(reshape(a1, (2, 2, 3))) == (2, 2, 3)
+
+    a2 = CircularArray(4, (2, 3))
+    @test isa(a2, CircularArray{Int, 2})
+end
+
+@testset "offset indices" begin
+    i = OffsetArray(1:5,-3)
+    a = CircularArray(i)
+    @test axes(a) == axes(i)
+    @test a[1] == 4
+    @test a[10] == a[-10] == a[0] == 3
+    @test a[-2:7] == [1:5; 1:5]
+    @test a[0:9] == [3:5; 1:5; 1:2]
+    @test a[1:10][-10] == 3
+    @test a[i] == OffsetArray([4,5,1,2,3],-3)
+
+    circ_a = circshift(a,3)
+    @test axes(circ_a) == axes(a)
+    @test circ_a[1:5] == 1:5
+
+    j = OffsetArray([true,false,true],1)
+    @test a[j] == [5,2]
+
+    data = reshape(1:9,3,3)
+    a = CircularArray(OffsetArray(data,-1,-1))
+    @test collect(a) == data
+    @test all(a[x,y] == data[mod1(x+1,3),mod1(y+1,3)] for x=-10:10, y=-10:10)
+    @test a[i,1] == CircularArray(OffsetArray([5,6,4,5,6],-2:2))
+    @test a[CartesianIndex.(i,i)] == CircularArray(OffsetArray([5,9,1,5,9],-2:2))
+    @test a[a .> 4] == 5:9
+end
